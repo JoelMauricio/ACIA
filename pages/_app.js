7908e4c75061e87b2905a63b4815aad7870ed272
Joel <1105039@est.intec.edu.do>
2023-03-20 23:14:53 -0400

    First changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import '@/styles/globals.css'
+import { useState } from 'react'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({
+  Component,
+  pageProps,
+}) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  )
 }
+
+export default App
