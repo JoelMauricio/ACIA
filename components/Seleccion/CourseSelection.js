14d0bb5f1ebd54180d0d40defd33cf7b14b20cc3
Joel <1105039@est.intec.edu.do>
2023-04-02 22:44:55 -0400

    fixed typo

--- components/Seleccion/CourseSelection.js
+++ components/Seleccion/CourseSelection.js
@@ -8,12 +8,12 @@ const SelectionPage = () => {
 
     return <>
         <div className='h-auto m-6 bg-transparent flex flex-col gap-1 '>
-            <h2 className="text-[1.5rem] font-bold text-mainBlac">Seleccion trimestre <span className="text-purBlue">periodo</span> </h2>
+            <h2 className="text-[1.5rem] font-bold text-mainBlac">Seleccion trimestre <span className="text-purBlue">período</span> </h2>
             <div className="flex flex-col w-full h-full gap-4">
                 <div className={section_format}>
                     <div className="rounded-md grid grid-flow-col justify-between h-10 w-full content-center">
                         <span className="font-semibold text-[18px]">Mi seleccion:</span>
-                        <button className="bg-purBlue text-boneWhite px-3 rounded-[0.3rem] h-fit">Guardar Seleccion</button>
+                        <button className="bg-purBlue text-boneWhite px-3 rounded-[0.3rem] h-fit">Guardar Selección</button>
                     </div>
                     <div className="h-full overflow-hidden overflow-y-auto px-2">
                         <div className="flex  flex-col w-full h-full bg-grid overflow-y-auto">
