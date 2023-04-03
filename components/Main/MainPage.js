c860776e3f6e2d465058919808943349e564ff1f
Joel <1105039@est.intec.edu.do>
2023-04-02 22:21:08 -0400

    Fixed design of Student History Page

--- components/Main/MainPage.js
+++ components/Main/MainPage.js
@@ -35,25 +35,27 @@ const Main = () => {
     ]);
 
     const { useProfileData } = useAuth()
-    const section_format = '"bg-boneWhite shadow-md w-full rounded-sm h-1/2 max-h-1/2 p-4 overflow-hidden'
+    const section_format = '"bg-boneWhite shadow-md w-full rounded-sm h-1/2 max-h-1/2 p-4 overflow-hidden first:h-fit'
 
     const profile = useProfileData();
 
     return <>
-        <div className='h-auto m-6 bg-transparent flex flex-col gap-1'>
+        <div className='h-auto m-6 bg-transparent flex flex-col gap-4'>
             <h2 className="text-[1.5rem] font-bold">Bienvenido de vuelta <span className="text-purBlue">{profile?.nombre ? (profile?.nombre) : ('Nombre Usuario')}</span></h2>
-            <div className={section_format}>
-                <div className="flex flex-wrap h-full">
-                    <Radial current={3.80} texto='Indice Trimestral' />
-                    <Radial current={3.90} texto='Indice General' />
+            <div className="flex flex-col w-full h-full gap-4">
+                <div className={section_format}>
+                    <div className="flex flex-wrap ">
+                        <Radial current={3.80} texto='Indice Trimestral' />
+                        <Radial current={3.90} texto='Indice General' />
+                    </div>
                 </div>
-            </div>
-            <div className={section_format}>
-                <span className="font-semibold text-[18px]">Proximos Eventos...</span>
-                <div className="flex flex-wrap ">
-                    {events.map((event, index) => (
-                        <Event_Card key={index} title={event.title} description={event.desciption} />
-                    ))}
+                <div className={section_format}>
+                    <span className="font-semibold text-[18px]">Proximos Eventos...</span>
+                    <div className="flex flex-wrap ">
+                        {events.map((event, index) => (
+                            <Event_Card key={index} title={event.title} description={event.desciption} />
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
