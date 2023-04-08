b7abc9869139f2d8c09a90c0d8bf58577e296596
C1fer <41005657+C1fer@users.noreply.github.com>
2023-04-08 04:04:59 -0400

    Added dynamic dropdown on Administrar Asignaturas

--- components/Admin/Course_AddCourse.js
+++ components/Admin/Course_AddCourse.js
@@ -2,12 +2,15 @@ import { Formik, Field, Form} from 'formik';
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import * as yup from 'yup';
 
-const AddCourse = ({}) => {
+const AddCourse = ({coursesData}) => {
   const supabase = useSupabaseClient()
   const label_format = "block tracking-wide text-gray-700 text-sm font-bold mb-2"
   const field_format = "appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
   const error_format = "mt-0.5 font-semibold text-xs text-red"
   const section_format = "flex flex-wrap md:w-1/2 mb-2 px-2"
+
+  let areas = coursesData.filter((course, index, self) => index === self.findIndex((s) => (s.id_area === course.id_area))); //Eliminar opciones duplicadas
+  let areaSort = areas.sort((a,b) => (a.Area_Academica.nombre < b.Area_Academica.nombre)? -1 : 1); //Ordenar opciones de forma ascendente
 
   return(
     <Formik
@@ -50,7 +53,7 @@ const AddCourse = ({}) => {
           <div className={section_format}>
             <label className={label_format} htmlFor="courseName">Nombre </label>
             <Field className= {field_format} id="courseName" name="courseName" placeholder="Asignatura..." />
-            {errors.courseName && touched.courseName ? (<p className={error_format}> {errors.courseName} </p>) : null}
+            {errors.courseName && touched.courseName ? (<p className={error_format}> {errors.courseName} </p>) : null} {/*Presentar error en pantalla*/}
           </div>
     
           <div className={section_format}>
@@ -61,15 +64,15 @@ const AddCourse = ({}) => {
     
           <div className={section_format}>
             <label className={label_format} htmlFor="courseCredits">No. Créditos</label>
-              <Field className={field_format} id="courseCredits" name ="courseCredits" as="select">
-              <option value=""></option>
-              <option value="0">0</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
+              <Field className={field_format} id="courseCredits" name ="courseCredits" as="select" >
+                <option value=""></option>
+                <option value="0">0</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
               </Field>
               {errors.courseCredits && touched.courseCredits ? (<p className={error_format}> {errors.courseCredits} </p>) : null}
           </div>
@@ -77,12 +80,8 @@ const AddCourse = ({}) => {
           <div className={section_format}>
             <label className={label_format} htmlFor="courseArea">Area Académica</label>
               <Field className={field_format} id="courseArea" name="courseArea"as="select">
-              <option value=""></option>
-              <option value="1">Ingeniería</option>
-              <option value="2">Economía y Negocios</option>
-              <option value="3">Ciencias de la Salud</option>
-              <option value="4">Ciencias Basicas y Ambientales</option>
-              <option value="5">Ciencias Sociales y Humanidades</option>
+                <option value=""></option>
+                {areaSort.map((area) => <option value={area.id_area}>{area.Area_Academica.nombre}</option>)}
               </Field>
               {errors.courseArea && touched.courseArea ? (<p className={error_format}> {errors.courseArea} </p>) : null}
           </div>
@@ -96,9 +95,9 @@ const AddCourse = ({}) => {
 //Validacion de entradas
 const validateSchema = yup.object().shape({
   courseName: yup.string().trim().matches(/^[A-Za-zÁÉÍÓÚáéíóúüñÑ()\- ]+$/, 'Introduzca un nombre utilizando letras y guiones (-).').required('¡Campo requerido!'),
-  courseCode : yup.string().length(6,'¡Introduzca un codigo de seis digitos!').matches(/^[a-zA-Z]{3}[0-9]{3}$/, '¡Codigo incorrecto!').required('!Campo requerido!'),
-  courseCredits : yup.string().required('Seleccione una opción.'),
-  courseArea : yup.string().required('Seleccione una opción.'),
+  courseCode : yup.string().length(6,'¡Introduzca un codigo de seis digitos!').matches(/^[a-zA-Z]{3}[0-9]{3}$/, '¡Codigo incorrecto!').required('¡Campo requerido!'),
+  courseCredits : yup.string().required('¡Opción requerida!'),
+  courseArea : yup.string().required('¡Opción requerida!.'),
 });
 
 export default AddCourse;
