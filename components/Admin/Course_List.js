acb91be0a6a25a9f8e84d18b9b40429ea4dd27f0
C1fer <41005657+C1fer@users.noreply.github.com>
2023-04-08 22:56:39 -0400

    Fixed pop-up styling

--- components/Admin/Course_List.js
+++ components/Admin/Course_List.js
@@ -70,7 +70,6 @@ const CourseList = ({ }) => {
                 <div className="flex flex-wrap gap-2 max-h-full">
                     {filteredCourses.map((course) => ( 
                         <div className="bg-white2 dark:bg-darkBD2 rounded-md grid grid-flow-col justify-between min-h-[45px] h-[90px] w-full shadow-[rgba(35,_37,_40,_0.18)_0px_3px_8px] py-2">
-
                         <div className="flex w-fit gap-4 text-[18px] items-center px-4">
                             <span className="ml-4 text-purBlue max-w-[100px] min-w-[100px]">{course.Area_Academica.nombre}</span>
                             <div className="w-[2px] h-[60%] bg-slate-300 justify-end mx-2" />
@@ -81,7 +80,7 @@ const CourseList = ({ }) => {
                             <div className="w-[2px] h-[60%] bg-slate-300" />
                             <Popup trigger={<button className="w-[30px] h-[30px]"> <Edit_iconv2 className="h-full w-full fill-red fill-" /></button>} closeOnDocumentClick={false} modal>
                                 {close => (
-                                    <div className="modal">
+                                    <div className="modal h-full w-full bg-white2 dark:bg-darkBD2 p-4 rounded-lg">
                                         <button className="bg-red text-white font-bold px-4 mx-1 mb-2 rounded" onClick={close}>&times;</button>
                                         <EditCourse key={course.id_asignatura} course_id={course.id_asignatura} name={course.nombre} area_id={course.id_area} code={course.codigo_asignatura} credits={course.creditos} optionsData={extractedAreas}/>
                                     </div>
