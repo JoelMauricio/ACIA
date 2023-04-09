6ee15d895c7161d855aeb5830cb18baacf698e0d
Oli Japa <olialexander1131@gmail.com>
2023-04-09 14:55:01 -0400

    lectura y mostrar seleccion funcionando

--- components/Seleccion/CourseSelection.js
+++ components/Seleccion/CourseSelection.js
@@ -1,11 +1,40 @@
 // import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 // import Event_Card from "../Main/EventCard";
 // import Radial from "../AcademicHistory/Radial";
 import SearchBar from "@/components/SearchBar";
+import { fetchSignatures } from "../hooks/fetchSignature";
+import Image from "next/image";
+import Edit_icon from '@/public/edit_icon.svg'
+
 
 const SelectionPage = () => {
+    const [signatures, setSignatures] = useState([])
+    const { fetchSelectionSignatures } = fetchSignatures()
     const section_format = 'bg-white2 shadow-[rgba(0,0,0,0.22)_0px_3px_8px] w-full rounded-sm h-1/2 px-4 py-2 overflow-hidden dark:bg-darkBD2'
+    const headerClass = "px-6 py-3 text-center text-xs font-semibold text-gray-500 uppercase tracking-wider"
+    const bodyClass = "border text-center px-2 whitespace-nowrap border-x-2 border-gray-200"
+
+    useEffect(() => {
+        if (signatures.length < 1) {
+            fetchSelectionSignatures().then((data) => {
+                setSignatures(data)
+            })
+        }
+        else {
+            setSignatures([])
+        }
+
+    }, [])
+
+    const handleSignature = (index) => {
+        const data = [...signatures]
+        data[index].check = true
+
+        setSignatures(data);
+        console.log(signatures)
+    };
+
 
     return <>
         <div className='h-auto m-6 bg-transparent flex flex-col gap-1 '>
@@ -18,7 +47,36 @@ const SelectionPage = () => {
                     </div>
                     <div className="h-full overflow-hidden overflow-y-auto px-2 mt-2">
                         <div className="flex  flex-col w-full h-full bg-grid overflow-y-auto dark:bg-darkGrid">
-                            {/* Agregar data grid view para los datos de la selección */}
+                            <table className="table-auto w-full">
+                                <thead className="bg-gray-50">
+                                    <tr>
+                                        <th className={headerClass}>Codigo de Asignatura</th>
+                                        <th className={headerClass}>Número de créditos</th>
+                                        <th className={headerClass}>Nombre</th>
+                                        <th className={headerClass}>Maestro</th>
+                                        <th className={headerClass}>Cupos</th>
+                                        <th className={headerClass}>Horario</th>
+                                        <th className={headerClass}>Aula</th>
+                                        <th className={headerClass}>Eliminar</th>
+                                    </tr>
+                                </thead>
+                                <tbody className="divide-y divide-gray-200">
+                                    {signatures?.filter((dato) => dato.check).map((dato, index) => (
+                                        <tr key={index}>
+                                            <td className={bodyClass}>{dato?.codigo_asignatura}</td>
+                                            <td className={bodyClass}>{dato?.creditos}</td>
+                                            <td className={bodyClass}>{dato?.nombre_asignatura}</td>
+                                            <td className={bodyClass}>{dato?.profesor}</td>
+                                            <td className={bodyClass}>{dato?.cupos}</td>
+                                            <td className={bodyClass}>{dato?.horario}</td>
+                                            <td className={bodyClass}>{dato?.aula}</td>
+                                            <td className={bodyClass}><button className="m-auto"><Edit_icon className="w-[30px] h-[30px] stroke-red" /></button></td>
+                                        </tr>
+                                    ))}
+
+
+                                </tbody>
+                            </table>
                         </div>
                         <div className="h-12 w-full bg-transparent" /> {/* just to get the space */}
                     </div>
@@ -29,8 +87,41 @@ const SelectionPage = () => {
                         <SearchBar text={"Buscar Asignatura"} className='h-full' />
                     </div>
                     <div className="h-full overflow-hidden overflow-y-auto px-2 mt-2">
-                        <div className="flex w-full h-full bg-grid dark:bg-darkGrid">
-                            {/* Agregar data grid view para los datos de las asignaturas */}
+                        <div className="w-full h-full bg-grid dark:bg-darkGrid">
+
+
+
+                            <table className="table-auto w-full">
+                                <thead className="bg-gray-50">
+                                    <tr>
+                                        <th className={headerClass}>Codigo de Asignatura</th>
+                                        <th className={headerClass}>Número de créditos</th>
+                                        <th className={headerClass}>Nombre</th>
+                                        <th className={headerClass}>Maestro</th>
+                                        <th className={headerClass}>Cupos</th>
+                                        <th className={headerClass}>Horario</th>
+                                        <th className={headerClass}>Aula</th>
+                                        <th className={headerClass}>Seleccion</th>
+                                    </tr>
+                                </thead>
+                                <tbody className="divide-y divide-gray-200">
+                                    {signatures?.map((dato, index) => (
+                                        <tr key={index}>
+                                            <td className={bodyClass}>{dato?.codigo_asignatura}</td>
+                                            <td className={bodyClass}>{dato?.creditos}</td>
+                                            <td className={bodyClass}>{dato?.nombre_asignatura}</td>
+                                            <td className={bodyClass}>{dato?.profesor}</td>
+                                            <td className={bodyClass}>{dato?.cupos}</td>
+                                            <td className={bodyClass}>{dato?.horario}</td>
+                                            <td className={bodyClass}>{dato?.aula}</td>
+                                            <td className={bodyClass}><input type="radio" checked={dato?.checked} onChange={() => handleSignature(index)}></input></td>
+
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+
+
                         </div>
                         <div className="h-12 w-full bg-transparent" /> {/* just to get the space */}
                     </div>
---
+++ components/hooks/fetchSignature.js
@@ -0,0 +1,91 @@
+import { useState, useEffect } from 'react';
+import { useSupabaseClient, useSession } from '@supabase/auth-helpers-react';
+import { useRouter } from 'next/router';
+import Cookies from 'js-cookie'
+import { useAuth } from '@/components/hooks/loginData';
+
+export function fetchSignatures() {
+
+    const supabase = useSupabaseClient();
+    const profile = useAuth().useProfileData();
+    const session = useSession();
+    const router = useRouter();
+    const token = Cookies.get("supabase-auth-token");
+
+    const fetchSelectionSignatures = async () => { //GET Asignaturas
+
+            try {
+
+                const { data } = await supabase
+                .from('Seccion')
+                .select('id_periodo')
+                .order('id_periodo', { ascending: false })
+                .limit(1);
+    
+                let { data: Seccion, error } = await supabase
+                    .from('Seccion')
+                    .select('*, Periodo(*), Asignatura(*), Persona(*), Aula(*)')
+                    .eq('id_periodo', data[0].id_periodo)
+
+                    console.log(Seccion)
+
+                    const newSeccion = Seccion.map(seccion => ({
+                        id_estudiante: profile?.id_Persona,
+                        id_seccion: seccion.id_seccion,
+                        id_periodo: seccion.id_periodo,
+                        id_asignatura: seccion.Asignatura.id_asignatura,
+                        codigo_asignatura: seccion.Asignatura.codigo_asignatura,
+                        creditos: seccion.Asignatura.creditos,
+                        nombre_asignatura: seccion.Asignatura.nombre,
+                        profesor: seccion.Persona.nombre,
+                        cupos: seccion.cupos,
+                        horario: seccion.horario,
+                        aula: seccion.Aula.nombre,
+                        check: false
+
+                      }));
+    
+                    console.log(newSeccion)
+    
+                return newSeccion
+            }
+            catch (error) {
+                alert(error.message);
+            }
+     
+
+
+    };
+
+    const uploadSelectedSignatures = async (rows) => {
+        try {
+
+            let { data: signatures } = await supabase
+                .from('Selecciones')
+                .select('*')
+                .eq('id_estudiante', profile?.id_Persona)
+                .eq('estado', 'seleccion');
+    
+            const signaturesIds = new Set(signatures.map((signature) => `${signature.id_asignatura}-${signature.id_periodo}-${signature.id_seccion}`));
+    
+            const filteredRows = rows.filter((row) => !signaturesIds.has(`${row.id_asignatura}-${row.id_periodo}-${row.id_seccion}`));
+    
+            const { data: insertedData, error } = await supabase
+                .from("Selecciones")
+                .insert(filteredRows);
+    
+            console.log(insertedData);
+            console.log(error);
+    
+        } catch (error) {
+            alert(error.message);
+        }
+    }
+    
+
+
+
+    return { fetchSelectionSignatures }
+
+}
+
