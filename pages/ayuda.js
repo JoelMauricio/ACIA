1dde4bb0456fe85cc9a5152565b54ed0c087302c
Joel <1105039@est.intec.edu.do>
2023-04-09 00:50:13 -0400

    fixedAcademic History Style fix

--- pages/ayuda.js
+++ pages/ayuda.js
@@ -13,28 +13,18 @@ const Ayuda = () => {
             "answer": "Nuestra política de devoluciones es que aceptamos devoluciones dentro de los 30 días posteriores a la compra. Para procesar una devolución, necesitarás proporcionar el recibo de compra o la confirmación del pedido. Si la devolución se debe a un error nuestro, cubriremos los costos de envío de la devolución. Si la devolución se debe a un error del cliente, el cliente es responsable de los costos de envío de la devolución. Una vez que hayamos recibido la devolución, procesaremos el reembolso dentro de 5-7 días hábiles.",
             "category": "Devoluciones"
         },
-        {
-            "question": "¿Cómo puedo contactar al servicio al cliente?",
-            "answer": "Puedes contactarnos de varias formas. Puedes llamarnos al número de teléfono que aparece en nuestro sitio web, enviar un correo electrónico a nuestro departamento de servicio al cliente o chatear con uno de nuestros representantes de servicio al cliente en vivo en nuestro sitio web. También puedes enviarnos un mensaje en cualquiera de nuestras redes sociales y te responderemos lo más pronto posible.",
-            "category": "Servicio al cliente"
-        },
-        {
-            "question": "¿Cuánto tiempo tarda el envío?",
-            "answer": "El tiempo de envío varía según la ubicación del cliente y el método de envío elegido. Para los pedidos nacionales, generalmente el envío tarda entre 3 y 5 días hábiles. Para los pedidos internacionales, el tiempo de envío puede ser mayor. Sin embargo, hacemos todo lo posible para procesar los pedidos lo más rápido posible y enviarlos de manera oportuna.",
-            "category": "Envío"
-        },
-        {
-            "question": "¿Aceptan pagos con tarjeta de crédito?",
-            "answer": "Sí, aceptamos pagos con tarjeta de crédito. Aceptamos todas las principales tarjetas de crédito, incluyendo Visa, Mastercard y American Express. También aceptamos pagos con PayPal y Apple Pay.",
-            "category": "Pagos"
-        },
-        {
-            "question": "¿Cuál es su política de privacidad?",
-            "answer": "Nos tomamos muy en serio la privacidad de nuestros clientes. Nunca compartimos ni vendemos información personal de nuestros clientes a terceros. Solo utilizamos la información que recopilamos para procesar los pedidos y mejorar la experiencia de compra en nuestro sitio web. Para obtener más información sobre nuestra política de privacidad, consulta la sección de privacidad en nuestro sitio web.",
-            "category": "Privacidad"
-        }
     ]);
 
+    async function GetFaqs() {
+        await fetch('../public/Ayuda.json')
+            .then(response => response.json())
+            .then(data => {
+                console.log(response)
+                setFaqs(data)
+            })
+    }
+
+    GetFaqs()
     const session = useSession();
     const router = useRouter();
 
