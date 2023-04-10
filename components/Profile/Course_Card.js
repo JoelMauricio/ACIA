2c7da53c9e33422c69446012981f581df9be0117
Joel <1105039@est.intec.edu.do>
2023-04-10 07:18:52 -0400

    fixed profile card stylling

--- components/Profile/Course_Card.js
+++ components/Profile/Course_Card.js
@@ -6,10 +6,10 @@ import 'reactjs-popup/dist/index.css';
 
 const CourseCard = ({ section, courseCode, courseName, grade }) => {
     return <>
-        <div className="rounded-md grid grid-flow-col justify-between min-h-[45px] h-fit w-full shadow-md dark:bg-darkGrid">
+        <div className="bg-white2 dark:bg-darkGrid rounded-md grid grid-flow-col justify-between min-h-[45px] h-[90px] w-full shadow-md py-2">
             <div className="flex w-fit gap-4 text-[18px] items-center px-4">
-                <span className="text-purBlue">{courseCode}-{section}</span>
-                <div className="w-[2px] h-[30px] bg-slate-300" />
+                <span className="ml-4 text-purBlue max-w-[100px] min-w-[100px]">{courseCode}-{section}</span>
+                <div className="w-[2px] h-[60%] bg-slate-300 justify-end mx-2" />
                 <span>{courseName}</span>
 
             </div>
