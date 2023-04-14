42073e50efe2af36c09cb5c785eaa45fd059819b
Oli Japa <olialexander1131@gmail.com>
2023-04-14 11:33:05 -0400

    ayuda bux fixed

--- pages/ayuda.js
+++ pages/ayuda.js
@@ -41,7 +41,7 @@ const Ayuda = () => {
 
     return (
         <>
-            <script className='bg-transparent w-[100px] h-[100px]' id="respondio__widget" src="https://cdn.respond.io/webchat/widget/widget.js?cId=b372bc8534673525c6917a1bd692bf421769338e675836b955a08bc7f5a307c6"></script>
+            <script className='bg-transparent dark:bg-transparent w-[100px] h-[100px]' style={{ background: 'transparent' }} id="respondio__widget" src="https://cdn.respond.io/webchat/widget/widget.js?cId=b372bc8534673525c6917a1bd692bf421769338e675836b955a08bc7f5a307c6"></script>
 
             <div className='pl-[260px] flex w-full h-screen bg-boneWhite dark:bg-darkBG'>
                 <Navbar />
